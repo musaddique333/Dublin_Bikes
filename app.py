3c481ca517e667030cbfc0533ba47dd073512100
musaddique333 <mohammedmusaddiquemogal@gmail.com>
2024-02-20 12:48:31 +0000

    second commit

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
+
 app = Flask(__name__, static_url_path='')
 
 @app.route("/")
 def hello_world():
     return app.send_static_file('index.html')
 
-
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=5000)
