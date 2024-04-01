b2e7b1687826695051391db8c719a03fd977bebe
abishek569 <159023454+abishek569@users.noreply.github.com>
2024-04-02 00:33:31 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,219 @@
+function initMap() {
+    const dublin = { lat: 53.350140, lng: -6.266155 };
+    // The map, centered at Dublin
+    var map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 12,
+        center: dublin,
+        styles: [
+            {
+                "featureType": "water",
+                "elementType": "geometry.fill",
+                "stylers": [
+                    {
+                        "color": "#d3d3d3"
+                    }
+                ]
+            },
+            {
+                "featureType": "transit",
+                "stylers": [
+                    {
+                        "color": "#808080"
+                    },
+                    {
+                        "visibility": "off"
+                    }
+                ]
+            },
+            {
+                "featureType": "road.highway",
+                "elementType": "geometry.stroke",
+                "stylers": [
+                    {
+                        "visibility": "on"
+                    },
+                    {
+                        "color": "#b3b3b3"
+                    }
+                ]
+            },
+            {
+                "featureType": "road.highway",
+                "elementType": "geometry.fill",
+                "stylers": [
+                    {
+                        "color": "#ffffff"
+                    }
+                ]
+            },
+            {
+                "featureType": "road.local",
+                "elementType": "geometry.fill",
+                "stylers": [
+                    {
+                        "visibility": "on"
+                    },
+                    {
+                        "color": "#ffffff"
+                    },
+                    {
+                        "weight": 1.8
+                    }
+                ]
+            },
+            {
+                "featureType": "road.local",
+                "elementType": "geometry.stroke",
+                "stylers": [
+                    {
+                        "color": "#d7d7d7"
+                    }
+                ]
+            },
+            {
+                "featureType": "poi",
+                "elementType": "geometry.fill",
+                "stylers": [
+                    {
+                        "visibility": "on"
+                    },
+                    {
+                        "color": "#ebebeb"
+                    }
+                ]
+            },
+            {
+                "featureType": "administrative",
+                "elementType": "geometry",
+                "stylers": [
+                    {
+                        "color": "#a7a7a7"
+                    }
+                ]
+            },
+            {
+                "featureType": "road.arterial",
+                "elementType": "geometry.fill",
+                "stylers": [
+                    {
+                        "color": "#ffffff"
+                    }
+                ]
+            },
+            {
+                "featureType": "landscape",
+                "elementType": "geometry.fill",
+                "stylers": [
+                    {
+                        "visibility": "on"
+                    },
+                    {
+                        "color": "#efefef"
+                    }
+                ]
+            },
+            {
+                "featureType": "road",
+                "elementType": "labels.text.fill",
+                "stylers": [
+                    {
+                        "color": "#696969"
+                    }
+                ]
+            },
+            {
+                "featureType": "administrative",
+                "elementType": "labels.text.fill",
+                "stylers": [
+                    {
+                        "visibility": "on"
+                    },
+                    {
+                        "color": "#737373"
+                    }
+                ]
+            },
+            {
+                "featureType": "poi",
+                "elementType": "labels.icon",
+                "stylers": [
+                    {
+                        "visibility": "off"
+                    }
+                ]
+            },
+            {
+                "featureType": "poi",
+                "elementType": "labels",
+                "stylers": [
+                    {
+                        "visibility": "off"
+                    }
+                ]
+            },
+            {
+                "featureType": "road.arterial",
+                "elementType": "geometry.stroke",
+                "stylers": [
+                    {
+                        "color": "#d6d6d6"
+                    }
+                ]
+            },
+            {
+                "featureType": "road",
+                "elementType": "labels.icon",
+                "stylers": [
+                    {
+                        "visibility": "off"
+                    }
+                ]
+            },
+            {},
+            {
+                "featureType": "poi",
+                "elementType": "geometry.fill",
+                "stylers": [
+                    {
+                        "color": "#dadada"
+                    }
+                ]
+            }
+        ]
+    });
+    // The marker, positioned at Dublin
+    const fehMarker = 'https://img.icons8.com/material-sharp/24/marker.png';
+    const marker = new google.maps.Marker({
+        position: dublin,
+        map: map,
+        icon: fehMarker
+    });
+
+    const infoWindowContent = `
+        <div class="feh-content">
+            <h3>Dublin</h3>
+            <p>Sample address in Dublin</p>
+        </div>
+    `;
+
+    const infoWindow = new google.maps.InfoWindow({
+        content: infoWindowContent
+    });
+
+    // Add click event listener to marker to open info window
+    marker.addListener('click', function() {
+        infoWindow.open(map, marker);
+    });
+}
+
+const addBtn = document.querySelector('.add-btn');
+addBtn.addEventListener('click', () => {
+  // Create a new input field for additional dropoff location
+  const newInput = document.createElement('input');
+  newInput.type = 'text';
+  newInput.placeholder = 'Enter additional dropoff location';
+  addBtn.parentNode.insertBefore(newInput, addBtn.nextSibling);
+});
+
+// Make initMap available globally so the Google Maps callback can invoke it
+window.initMap = initMap;
