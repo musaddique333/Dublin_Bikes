6b764c51bcdc5a08f7df752b01cd40ed75b4c635
abishek569 <159023454+abishek569@users.noreply.github.com>
2024-04-18 18:10:44 +0700

    Added a feature which calculates the gust speed.

--- statistics.js
+++ statistics.js
@@ -300,3 +300,272 @@ function plot_wind(bikes, bike_stands, X) {
     if (windStandsChart) windStandsChart.destroy();
     windStandsChart = new Chart(wind_stands, standsConfig);
 }
+function plot_gust(bikes, bike_stands, X) {
+    const gust_bikes = document.querySelector(".gust-bikes").getContext('2d');
+    const gust_stands = document.querySelector(".gust-stands").getContext('2d');
+
+    const labels = X;
+
+    const data = (plot, plotData) => ({
+        labels: labels,
+        datasets: [{
+            label: plot,
+            data: plotData,
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.5)',
+                'rgba(75, 192, 192, 0.5)',
+                'rgba(255, 205, 86, 0.5)',
+                'rgba(201, 203, 207, 0.5)',
+                'rgba(54, 162, 235, 0.5)'
+            ]
+        }]
+    });
+
+    var bikesData = data('Bikes', bikes);
+    var bikesConfig = {
+        type: 'polarArea',
+        data: bikesData,
+        options: {
+            responsive: true,
+            scales: {
+                r: {
+                    pointLabels: {
+                        display: true,
+                        centerPointLabels: true,
+                        font: {
+                            size: 18
+                        }
+                    }
+                },
+                x: {
+                    type: 'category',
+                    position: 'bottom',
+                    title: {
+                        display: true,
+                        text: `Gust Speeds`,
+                        color: '#bd7ebe',
+                        font: {
+                            size: 20
+                        }
+                    },
+                    ticks: {
+                        color: 'rgba(255, 255, 255, 0.6)',
+                        font: {
+                            size: 12
+                        }
+                    },
+                    grid: {
+                        color: 'rgba(255, 255, 255, 0.6)',
+                    },
+                },
+                y: {
+                    type: 'linear',
+                    position: 'left',
+                    title: {
+                        display: true,
+                        text: 'Bikes Availability',
+                        color: '#bd7ebe',
+                        font: {
+                            size: 20
+                        }
+                    },
+                    ticks: {
+                        color: 'rgba(255, 255, 255, 0.6)',
+                        font: {
+                            size: 12
+                        }
+                    },
+                    grid: {
+                        color: 'rgba(255, 255, 255, 0.6)',
+                    },
+                },
+                y1: {
+                    type: 'linear',
+                    display: true,
+                    position: 'right',
+                    grid: {
+                        drawOnChartArea: false,
+                    },
+                    title: {
+                        display: true,
+                        text: 'Bikes Availability',
+                        color: '#bd7ebe',
+                        font: {
+                            size: 20
+                        }
+                    },
+                    ticks: {
+                        color: 'rgba(255, 255, 255, 0.6)',
+                        font: {
+                            size: 12
+                        }
+                    },
+                },
+            },
+            plugins: {
+                legend: {
+                    display: true,
+                    align: 'center',
+                    labels: {
+                        font: {
+                            size: 20
+                        },
+                        color: "#bd7ebe",
+                    }
+                },
+                title: {
+                    display: true,
+                    text: `Availability Of Bikes Based On Gust Speeds`,
+                    font: {
+                        size: 25,
+                        weight: 'bold',
+                    },
+                    color: 'rgba(255, 255, 255, 1)',
+                    position: 'top',
+                },
+                zoom: {
+                    zoom: {
+                        wheel: {
+                            enabled: true,
+                        },
+                        pinch: {
+                            enabled: true
+                        },
+                        mode: 'xy',
+                    },
+                    pan: {
+                        enabled: true,
+                        mode: 'xy',
+                    }
+                }
+            },
+        }
+    };
+
+    if (gustBikesChart) gustBikesChart.destroy();
+    gustBikesChart = new Chart(gust_bikes, bikesConfig);
+
+    var standsData = data('Stands', bike_stands);
+    var standsConfig = {
+        type: 'polarArea',
+        data: standsData,
+        options: {
+            responsive: true,
+            scales: {
+                r: {
+                    pointLabels: {
+                        display: true,
+                        centerPointLabels: true,
+                        font: {
+                            size: 18
+                        }
+                    }
+                },
+                x: {
+                    type: 'category',
+                    position: 'bottom',
+                    title: {
+                        display: true,
+                        text: `Gust Speeds`,
+                        color: '#bd7ebe',
+                        font: {
+                            size: 20
+                        }
+                    },
+                    ticks: {
+                        color: 'rgba(255, 255, 255, 0.6)',
+                        font: {
+                            size: 12
+                        }
+                    },
+                    grid: {
+                        color: 'rgba(255, 255, 255, 0.6)',
+                    },
+                },
+                y: {
+                    type: 'linear',
+                    position: 'left',
+                    title: {
+                        display: true,
+                        text: 'Stands Availability',
+                        color: '#bd7ebe',
+                        font: {
+                            size: 20
+                        }
+                    },
+                    ticks: {
+                        color: 'rgba(255, 255, 255, 0.6)',
+                        font: {
+                            size: 12
+                        }
+                    },
+                    grid: {
+                        color: 'rgba(255, 255, 255, 0.6)',
+                    },
+                },
+                y1: {
+                    type: 'linear',
+                    display: true,
+                    position: 'right',
+                    grid: {
+                        drawOnChartArea: false,
+                    },
+                    title: {
+                        display: true,
+                        text: 'Stands Availability',
+                        color: '#bd7ebe',
+                        font: {
+                            size: 20
+                        }
+                    },
+                    ticks: {
+                        color: 'rgba(255, 255, 255, 0.6)',
+                        font: {
+                            size: 12
+                        }
+                    },
+                },
+            },
+            plugins: {
+                legend: {
+                    display: true,
+                    align: 'center',
+                    labels: {
+                        font: {
+                            size: 20
+                        },
+                        color: "#bd7ebe",
+                    }
+                },
+                title: {
+                    display: true,
+                    text: `Availability Of Stands Based On Gust Speeds`,
+                    font: {
+                        size: 25,
+                        weight: 'bold',
+                    },
+                    color: 'rgba(255, 255, 255, 1)',
+                    position: 'top',
+                },
+                zoom: {
+                    zoom: {
+                        wheel: {
+                            enabled: true,
+                        },
+                        pinch: {
+                            enabled: true
+                        },
+                        mode: 'xy',
+                    },
+                    pan: {
+                        enabled: true,
+                        mode: 'xy',
+                    }
+                }
+            },
+        },
+    };
+
+    if (gustStandsChart) gustStandsChart.destroy();
+    gustStandsChart = new Chart(gust_stands, standsConfig);
+}
