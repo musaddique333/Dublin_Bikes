ff3e80d8c57fee3091914cb3eda894b3c8c6f8b0
abishek569 <159023454+abishek569@users.noreply.github.com>
2024-04-18 18:07:44 +0700

    Added a function to plot wind.

--- statistics.js
+++ statistics.js
@@ -39,3 +39,264 @@ async function get_data() {
 document.addEventListener("DOMContentLoaded", () => {
     get_data();
 });
+function plot_wind(bikes, bike_stands, X) {
+    const wind_bikes = document.querySelector(".wind-bikes").getContext('2d');
+    const wind_stands = document.querySelector(".wind-stands").getContext('2d');
+
+    const labels = X;
+
+    const data = (plot, plotData) => ({
+        labels: labels,
+        datasets: [{
+            label: plot,
+            data: plotData,
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(255, 159, 64, 0.2)',
+                'rgba(255, 205, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(153, 102, 255, 0.2)'
+            ],
+            borderColor: [
+                'rgb(255, 99, 132)',
+                'rgb(255, 159, 64)',
+                'rgb(255, 205, 86)',
+                'rgb(75, 192, 192)',
+                'rgb(54, 162, 235)',
+                'rgb(153, 102, 255)'
+            ],
+            borderWidth: 1
+        }]
+    });
+
+    var bikesData = data('bikes', bikes);
+    var bikesConfig = {
+        type: 'bar',
+        data: bikesData,
+        options: {
+            responsive: true,
+            scales: {
+                x: {
+                    type: 'category',
+                    position: 'bottom',
+                    title: {
+                        display: true,
+                        text: `Wind Speeds`,
+                        color: '#bd7ebe',
+                        font: {
+                            size: 20
+                        }
+                    },
+                    ticks: {
+                        color: 'rgba(255, 255, 255, 0.6)',
+                        font: {
+                            size: 12
+                        }
+                    },
+                    grid: {
+                        color: 'rgba(255, 255, 255, 0.6)',
+                    },
+                },
+                y: {
+                    type: 'linear',
+                    position: 'left',
+                    title: {
+                        display: true,
+                        text: 'Bikes Availability',
+                        color: '#bd7ebe',
+                        font: {
+                            size: 20
+                        }
+                    },
+                    ticks: {
+                        color: 'rgba(255, 255, 255, 0.6)',
+                        font: {
+                            size: 12
+                        }
+                    },
+                    grid: {
+                        color: 'rgba(255, 255, 255, 0.6)',
+                    },
+                },
+                y1: {
+                    type: 'linear',
+                    display: true,
+                    position: 'right',
+                    grid: {
+                        drawOnChartArea: false,
+                    },
+                    title: {
+                        display: true,
+                        text: 'Bikes Availability',
+                        color: '#bd7ebe',
+                        font: {
+                            size: 20
+                        }
+                    },
+                    ticks: {
+                        color: 'rgba(255, 255, 255, 0.6)',
+                        font: {
+                            size: 12
+                        }
+                    },
+                },
+            },
+            plugins: {
+                legend: {
+                    display: true,
+                    align: 'center',
+                    labels: {
+                        font: {
+                            size: 20
+                        },
+                        color: "#bd7ebe",
+                    }
+                },
+                title: {
+                    display: true,
+                    text: `Availability Of Bikes Based On Wind Speeds`,
+                    font: {
+                        size: 30,
+                        weight: 'bold',
+                    },
+                    color: 'rgba(255, 255, 255, 1)',
+                    position: 'top',
+                },
+                zoom: {
+                    zoom: {
+                        wheel: {
+                            enabled: true,
+                        },
+                        pinch: {
+                            enabled: true
+                        },
+                        mode: 'xy',
+                    },
+                    pan: {
+                        enabled: true,
+                        mode: 'xy',
+                    }
+                }
+            },
+        }
+    };
+
+    if (windBikesChart) windBikesChart.destroy();
+    windBikesChart = new Chart(wind_bikes, bikesConfig);
+
+    var standsData = data('Stands', bike_stands);
+    var standsConfig = {
+        type: 'bar',
+        data: standsData,
+        options: {
+            responsive: true,
+            scales: {
+                x: {
+                    type: 'category',
+                    position: 'bottom',
+                    title: {
+                        display: true,
+                        text: `Wind Speeds`,
+                        color: '#bd7ebe',
+                        font: {
+                            size: 20
+                        }
+                    },
+                    ticks: {
+                        color: 'rgba(255, 255, 255, 0.6)',
+                        font: {
+                            size: 12
+                        }
+                    },
+                    grid: {
+                        color: 'rgba(255, 255, 255, 0.6)',
+                    },
+                },
+                y: {
+                    type: 'linear',
+                    position: 'left',
+                    title: {
+                        display: true,
+                        text: 'Stands Availability',
+                        color: '#bd7ebe',
+                        font: {
+                            size: 20
+                        }
+                    },
+                    ticks: {
+                        color: 'rgba(255, 255, 255, 0.6)',
+                        font: {
+                            size: 12
+                        }
+                    },
+                    grid: {
+                        color: 'rgba(255, 255, 255, 0.6)',
+                    },
+                },
+                y1: {
+                    type: 'linear',
+                    display: true,
+                    position: 'right',
+                    grid: {
+                        drawOnChartArea: false,
+                    },
+                    title: {
+                        display: true,
+                        text: 'Stands Availability',
+                        color: '#bd7ebe',
+                        font: {
+                            size: 20
+                        }
+                    },
+                    ticks: {
+                        color: 'rgba(255, 255, 255, 0.6)',
+                        font: {
+                            size: 12
+                        }
+                    },
+                },
+            },
+            plugins: {
+                legend: {
+                    display: true,
+                    align: 'center',
+                    labels: {
+                        font: {
+                            size: 20
+                        },
+                        color: "#bd7ebe",
+                    }
+                },
+                title: {
+                    display: true,
+                    text: `Availability Of Stands Based On Wind Speeds`,
+                    font: {
+                        size: 30,
+                        weight: 'bold',
+                    },
+                    color: 'rgba(255, 255, 255, 1)',
+                    position: 'top',
+                },
+                zoom: {
+                    zoom: {
+                        wheel: {
+                            enabled: true,
+                        },
+                        pinch: {
+                            enabled: true
+                        },
+                        mode: 'xy',
+                    },
+                    pan: {
+                        enabled: true,
+                        mode: 'xy',
+                    }
+                }
+            },
+        },
+    };
+
+    if (windStandsChart) windStandsChart.destroy();
+    windStandsChart = new Chart(wind_stands, standsConfig);
+}
